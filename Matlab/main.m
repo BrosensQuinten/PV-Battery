7a7630e64329666a686acfe970ac95e165012afe
BrosensQuinten <quinten.brosens@student.kuleuven.be>
2021-02-23 16:47:03 +0100

    interpolate
    code which interpolates the missing globrad and diffrad from next and previous month

--- Matlab/main.m
+++ Matlab/main.m
@@ -3,3 +3,6 @@ path = [pwd , '\Data\Irradiance_data.mat'];
 irradiance_data = load(path);
 irradiance_data = irradiance_data.Irradiancedata;
 irradiance_data_filled = irradiance_fill(irradiance_data);
+
+
+%%
