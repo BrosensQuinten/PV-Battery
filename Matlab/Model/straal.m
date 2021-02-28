d76ad740f95fad8ea33f85bd5c76f0c63173240b
merijnvd <merijnvandeyck@hotmail.com>
2021-02-28 12:00:20 +0100

    functies fixed

--- Matlab/Model/straal.m
+++ Matlab/Model/straal.m
@@ -1,4 +1,4 @@
-function [ray,date_time,intensity,azimuth,elevation,glob_rad,diffusion,len]=straal()
+function ray=straal()
 filename1 = 'Solar_Pos_Final.xlsx';
 solar_position = readtable(filename1);
 Azimuth = solar_position{:,2};
@@ -14,12 +14,9 @@ diff_rad = irr_data{:,3};
 len = size(date_time,1);
 intensity = zeros(len,1);
 for i=1:len
-    gl = glob_rad(i,1);
-    ang = Elevation(i,1);
-    intensity(i,1) = gl/sin(pi*ang/180);
+    intensity(i,1) = glob_rad(i,1)/sin(pi*Elevation(i,1)/180);
 end
-azimuth = Azimuth(1:len,1);
-elevation = Elevation(1:len,1);
-diffusion = diff_rad;
-ray = table(date_time, intensity, azimuth, elevation, glob_rad,diffusion);
+azimuth = Azimuth(:,1);
+elevation = Elevation(:,1);
+ray = table(date_time, intensity, azimuth, elevation, glob_rad,diff_rad);
 end
