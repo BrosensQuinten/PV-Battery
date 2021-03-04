328767c5aa95a50c00d93d93d405239e7c7ae86d
ronald420 <78918977+ronald420@users.noreply.github.com>
2021-03-04 15:57:33 +0100

    user_interface modified

--- Matlab/Model/user_interface.m
+++ Matlab/Model/user_interface.m
@@ -1,8 +1,8 @@
 %                        User interface                      %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% [irr_sf,tot_sf]  = south_face(45,1);
-% [irr_ew,tot_ew] = east_west(45,1);
+% ray = straal(); % kan je 1 keer runnen en dan scenario's vergelijken.
+
 roof = input('Flat (1) or gable roof (2)?: '); 
 if  isempty(roof) == 1
     return;
@@ -10,8 +10,9 @@ elseif (roof ~= 1) && (roof ~= 2)
     disp('That input is unvalid! Answer with "1" or "2" ');
 end
 
-roof_area = 60; 
-% roof_angle = ; %only used for gable roof
+roof_area = 60; % updaten met merijn
+roof_angle = 30; %only used for gable roof update
+surface_area = roof_area; %oppervlakte aan zonnepanelen, kan later nog variabel worden miss?
 
 orientation = input('In which direction are the PV panels installed? Possible answers: 1 (South), 2 (EW): ');
 if  isempty(orientation) == 1
@@ -20,27 +21,32 @@ elseif (orientation ~= 1) && (orientation ~= 2)
     disp('That input is unvalid! Answer with "1" or "2" ');
 end
 
+tariff = input('Which tariff is used? Prosumer (1) or self_consuming (2)?: ');
+if  isempty(tariff) == 1
+    return;
+elseif (tariff ~= 1) && (tariff ~= 2)
+    disp('That input is unvalid! Answer with "1" or "2" ');
+end
+
+
+
 if roof == 1 && orientation == 1
     disp('The most optimal angle is 43 degrees. (Angle_Optimization');
     angle = 43;
-elseif roof == 1 && orientation == 0
-    disp('The most optimal angle is 35 degrees. (Angle_Optimization)')
+    irr = south_face(angle,surface_area,ray);
+elseif roof == 1 && orientation == 2
+    disp('The most optimal angle is 35 degrees. Angle_Optimization.m')
     angle = 35;
+elseif roof == 2 && orientation == 1
+    irr = south_face(roof_angle,surface_area,ray);
+else
+    irr = east_west(roof_angle,surface_area,ray);
 end
 
-surface_area = roof_area;
-%possible addition: which tariff is used?
-%possible addition: what is the efficiency of the solar panel?
 disp('Thank you. Performing calculations...');
 
-ray = straal();
 
-%calculate received power per minute
-if orientation == 1
-    irr = south_face(angle,surface_area,ray);
-else
-    irr = east_west(angle,surface_area,ray);
-end
+
 
 
 %Conversion to actual generated power (depends on efficiency solar panels);
