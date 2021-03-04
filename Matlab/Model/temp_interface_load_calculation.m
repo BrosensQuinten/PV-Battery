bb2127498f5fbc69b0d5694d6bba97304205e67e
merijnvd <merijnvandeyck@hotmail.com>
2021-03-04 18:02:08 +0100

    Bijna alle pieken weg

--- Matlab/Model/temp_interface_load_calculation.m
+++ Matlab/Model/temp_interface_load_calculation.m
@@ -3,4 +3,4 @@ load_15m = readtable(filename);
 %[tot_day,avg_day]=load_day(load_15m,28,09);
 %[tot_year,avg_year] = load_year(load_15m);
 %[pf,injectie,consumptie]=Power_Flow(sunpower_maxeon_3,irr, load_15m,20);
-intensity = ray(:,2);
+straal()
