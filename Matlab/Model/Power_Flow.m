4eac7f3d5dd21ed5746f9d0ced59079afa11df2d
ronald420 <78918977+ronald420@users.noreply.github.com>
2021-03-17 16:51:28 +0100

    Adaptation power flow

--- Matlab/Model/Power_Flow.m
+++ Matlab/Model/Power_Flow.m
@@ -1,21 +1,42 @@
-function [pf,injectie,consumptie] = Power_Flow(Solar_panel,irr,load)
-eff = Solar_panel.efficiency;
-gen = eff/100*irr/1000; % gedeeld door 1000 om in kW te zetten
-length = size(gen,1);
-pf = zeros(length,1);
-injectie = 0;
-consumptie = 0;
-counter = 0;
+function [pf,injectie,consumptie] = Power_Flow(Efficiency,irr,load)
 
-for i = 1:length
+
+month_indices = [1 44641 84961 129541 172741 217381 260581 305221 349861 393061 437761 480961 525601];
+
+
+length = size(irr,1);
+pf = zeros(length,1);
+
+index = 1;
+
+for i=1:12
     
-    pf(i,1) = (gen(i,1) - load(counter+1,1));
+    eff = Efficiency(i);
+    gen = eff/100*irr/1000; % gedeeld door 1000 om in kW te zetten
     
-    if pf(i,1) > 0
-        injectie = injectie + pf(i,1)/60; % waarom deling door 60?
-    else 
-        consumptie = consumptie - pf(i,1)/60;
+    injectie = 0;
+    consumptie = 0;
+    
+    j = 0;
+    counter = 1;
+    
+    while j == 0
+
+        pf(index,1) = (gen(index,1) - load(counter+1,1));
+
+        if pf(index,1) > 0
+            injectie = injectie + pf(index,1)/60; 
+        else 
+            consumptie = consumptie - pf(index,1)/60;
+        end
+        counter = floor(index/15);
+        index = index + 1;
+        if ismember(index,month_indices)
+            j = 1;
+        end
+            
     end
-    counter = floor(i/15);
+   
+    
 end
 
--- Matlab/Model/temp_interface_load_calculation.m
+++ Matlab/Model/temp_interface_load_calculation.m
@@ -13,17 +13,3 @@ eff = 0.17;
 
 power_diff_15m = eff*irr_15m/1000 - load_15m.Load_kW; %positive = power into grid, negative = power drawn from grid
 
-%this code averages the irradiance over each of the months.
-% month_indices = [1 2977 5666 8641 11521 14497 17377 20353 23329 26209 29185 32065 35041]; %indices in load/irr matrix of the start of each month
-month_indices = [1 44641 84961 129541 172741 217381 260581 305221 349861 393061 437761 480961 525601];
-irr_monthly = zeros(1,1);
-
-
-for i = 1:size(month_indices,2)-1
-    irr_monthly = vertcat(irr_monthly, mean(nonzeros(irr(month_indices(i): month_indices(i+1)-1))));
-end
-irr_monthly(1,:) = [];
-
-
-[eff,T]=Efficiency(LG_Neon_5,irr_monthly);
-T = T -273.15;
